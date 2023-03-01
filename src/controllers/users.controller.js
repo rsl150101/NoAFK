870f11806ada8d72ff608792431913ea93a651be
Kyeongjin-Park <cloud1178@naver.com>
2023-03-01 23:33:07 +0900

    [추가] users 전체 유저 조회

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,6 +2,19 @@ const UserService = require('../services/users.service');
 
 class UserController {
   userService = new UserService();
+  
+  // 회원 전체 조회 (email, nickname, auth_level, test_result, introduction, expired_at)
+  getAllUserInfo = async (req, res) => {
+    try {
+      const findAllUserInfo = await this.userService.findAllUserInfo();
+
+      res.status(200).json(findAllUserInfo);
+    } catch (error) {
+      console.error(error);
+      return res.status(400).json({ errorMessage: '회원 전체 조회 실패.' })
+    }
+  }
+
 }
 
 module.exports = UserController;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,4 +4,8 @@ const UserController = require('../controllers/users.controller');
 const router = express.Router();
 const userController = new UserController();
 
+// 회원 전체 조회
+router.get('/', userController.getAllUserInfo);
+
+
 module.exports = router;
