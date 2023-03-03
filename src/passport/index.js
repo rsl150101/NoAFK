1b814c7a15f5bb1e44c8de937e6149fb019ca134
Jeongjiw00 <wldn0229@naver.com>
2023-03-04 01:20:42 +0900

    [삭제] console.log 삭제

--- src/passport/index.js
+++ src/passport/index.js
@@ -41,7 +41,6 @@ module.exports = (app) => {
             done(null, newUser); // 회원가입하고 로그인 인증 완료
           }
         } catch (error) {
-          console.error(error);
           done(error);
         }
       }
