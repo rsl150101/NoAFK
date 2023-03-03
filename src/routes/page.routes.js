12c9492ad379004822c1c9ae951a7a478e627ef2
Haksoo Ji <mintzzz1009@gmail.com>
2023-03-04 06:58:53 +0900

    [수정] 페이지 라우터에서 미들웨어부분 임시 삭제

--- src/routes/page.routes.js
+++ src/routes/page.routes.js
@@ -13,7 +13,10 @@ router.get('/users');
 router.get('/projects');
 router.get('/teams/:teamid', (req, res, next) => {
   // 추후 auth 미들웨어로 추가
+
   return res.render('myteam');
 });
 
 module.exports = router;
+
+const user = res.local.user;
