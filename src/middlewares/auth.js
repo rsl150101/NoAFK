849c48d9f5319eba019a4015b98f94fbf22f0b77
Jeongjiw00 <wldn0229@naver.com>
2023-03-03 21:12:58 +0900

    [수정] 미들웨어 수정

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -15,6 +15,11 @@ const notLogin = async (req, res, next) => {
 
     let [authType, authToken] = cookie.split('=');
 
+    // 소셜로그인 인증
+    if (authToken.includes('connect.sid')) {
+      authToken = authToken.split(';')[0];
+    }
+
     if (!authToken || authType !== 'accessToken') {
       return next();
     }
@@ -33,7 +38,8 @@ const notLogin = async (req, res, next) => {
     next();
   } catch (error) {
     res.clearCookie('accessToken');
-    return res.status(401).json({ message: '로그인 후 이용가능합니다.' });
+    res.clearCookie('connect.sid');
+    return;
   }
 };
 
