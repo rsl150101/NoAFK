1b6aa36a18540c8cca8c8b232e268ed94acfa71a
go-tiger <inter_tiger_@naver.com>
2023-03-03 12:29:11 +0900

    [추가] 회원관리 렌더페이지

--- src/routes/page.routes.js
+++ src/routes/page.routes.js
@@ -4,7 +4,7 @@ const TeamController = require('../controllers/teams.controller');
 const ProjectController = require('../controllers/projects.controller');
 
 const router = express.Router();
-const userController = new UserController();
+const usersController = new UserController();
 const teamController = new TeamController();
 const projectController = new ProjectController();
 
@@ -12,5 +12,6 @@ router.get('/');
 router.get('/users');
 router.get('/projects');
 router.get('/teams');
+router.get('/adminUser', usersController.renderAdminUserPage);
 
 module.exports = router;
