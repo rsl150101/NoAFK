f22bbe29727c6f3a9d2a80fe49684c5fccd2e2df
go-tiger <inter_tiger_@naver.com>
2023-03-21 03:03:27 +0900

    [추가] 어드민 라우터에 회원관리 추가

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const ProjectsController = require('../controllers/projects.controller');
+const UsersController = require('../controllers/users.controller');
 
 const router = express.Router();
 const projectsController = new ProjectsController();
+const usersController = new UsersController();
 
 router.get('/projects', projectsController.getOffsetBasedProjects);
 router.delete('/projects/:id', projectsController.hardDeleteProject);
+
+//* 백오피스 - 회원관리
+router.get('/users', usersController.renderAdminUserPage);
 
 module.exports = router;
