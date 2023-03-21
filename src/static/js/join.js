a89899299af65e9d5871652ab9a3b54f6b53981a
Jeongjiw00 <wldn0229@naver.com>
2023-03-21 13:36:26 +0900

    [수정] 회원가입페이지 프론트

--- src/static/js/join.js
+++ src/static/js/join.js
@@ -1,6 +1,52 @@
-const joinBtn = document.getElementById('join-btn');
+const joinForm = document.getElementById('join-box');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const nicknameInput = document.getElementById('nickname');
 
-joinBtn.addEventListener('click', () => {
+emailInput.addEventListener('change', () => {
+  // 이메일: aaa@aaa.aaa
+  const emailCheck = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
+  if (!emailCheck.test(emailInput.value)) {
+    return (document.getElementById('emailMsg').style.display = 'block');
+  }
+  return (document.getElementById('emailMsg').style.display = 'none');
+});
+
+passwordInput.addEventListener('change', () => {
+  // 비밀번호: 영어대소문자숫자
+  const passwordCheck = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,20}$/;
+
+  if (!passwordCheck.test(passwordInput.value)) {
+    return (document.getElementById('passwordMsg').style.display = 'block');
+  }
+
+  document.getElementById('passwordMsg').style.display = 'none';
+
+  const passwordTwice = document.getElementById('passwordCheck');
+
+  passwordTwice.addEventListener('change', () => {
+    if (passwordInput.value !== passwordTwice.value) {
+      return (document.getElementById('passwordCheckMsg').style.display =
+        'block');
+    }
+
+    return (document.getElementById('passwordCheckMsg').style.display = 'none');
+  });
+});
+
+nicknameInput.addEventListener('change', () => {
+  // 닉네임:한글포함영어대소문자숫자
+  const nicknameCheck = /^[A-za-z0-9가-힣]*.{1,20}$/;
+
+  if (!nicknameCheck.test(nicknameInput.value)) {
+    return (document.getElementById('nicknameMsg').style.display = 'block');
+  }
+  return (document.getElementById('nicknameMsg').style.display = 'none');
+});
+
+joinForm.addEventListener('submit', (e) => {
+  e.preventDefault();
   join();
 });
 
