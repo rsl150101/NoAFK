a4adb5bddd338f3c7d39da1d673f7b18b8c85702
Jeongjiw00 <wldn0229@naver.com>
2023-03-26 01:32:50 +0900

    [삭제] 학수님 console.log

--- src/static/js/chatting.js
+++ src/static/js/chatting.js
@@ -1,7 +1,6 @@
 const path = location.pathname.split('/');
 const { 2: teamId, 3: memberId } = path;
 const nickname = document.querySelector('#userNickname').textContent;
-console.log(nickname);
 
 const socket = io.connect(`http://localhost:3000/chat`);
 
