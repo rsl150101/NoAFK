b405f3b83ab3922eed9d1ae6f84f35a3a963f7e6
Haksoo Ji <mintzzz1009@gmail.com>
2023-03-27 09:49:50 +0900

    [수정] 채팅 기능 개선 및 스타일 추가

--- src/socket.js
+++ src/socket.js
@@ -5,40 +5,39 @@ module.exports = (server, app) => {
   app.set('io', io);
   const chat = io.of('/chat');
   const notice = io.of('/notice'); // 추후 전체 알림 기능 등 사용
-
-  chat.on('connection', (socket) => {
-    socket.emit('socketId', socket.id);
-
+  chat.on('connection', async (socket) => {
     const req = socket.request;
     const {
       headers: { referer },
     } = req;
-    const path = referer.split('/chat/')[1].split('/');
-    let { 0: chatId, 1: memberId } = path;
-    req.nickname = '짱구'; // 임시구현
 
-    if (memberId) {
-      const whisperRoomId = [req.myId, memberId]
-        .sort((a, b) => a - b)
-        .join('&');
-      chatId = whisperRoomId;
+    const path = referer.split('/chat/')[1].split('/');
+    let { 0: teamId, 1: myNickname, 2: memberNickname } = path;
+
+    let chatId = teamId;
+    if (memberNickname) {
+      chatId = [teamId, myNickname, memberNickname].sort().join('&');
     }
+
     socket.join(chatId);
 
     socket.to(chatId).emit('join', {
       type: 'notice',
-      params: { value: `${req.nickname}님이 입장하셨습니다.` },
+      params: {
+        value: `${decodeURIComponent(myNickname)}님이 입장하셨습니다.`,
+      },
     });
 
-    socket.on('message', (msg) => {
-      socket.emit('message', msg);
-      socket.to(chatId).emit('message', msg);
+    socket.on('message', (message) => {
+      socket.to(chatId).emit('message', message);
     });
 
     socket.on('disconnect', () => {
       socket.to(chatId).emit('join', {
         type: 'notice',
-        params: { value: `${req.nickname}님이 퇴장하셨습니다.` },
+        params: {
+          value: `${decodeURIComponent(myNickname)}님이 퇴장하셨습니다.`,
+        },
       });
       socket.leave(chatId);
     });
