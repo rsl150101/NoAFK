39733e5508415413a036f01ff9b350fe9fcea545
Haksoo Ji <mintzzz1009@gmail.com>
2023-03-28 01:00:11 +0900

    [수정] innerHTML -> insertAdjacentHTML 변경 / 리더 직책 수정 불가로 변경

--- src/static/js/myteam/teamEditMember.js
+++ src/static/js/myteam/teamEditMember.js
@@ -6,37 +6,56 @@ function updateMember(memberId) {
   const thTask = document.querySelector(`#task-${memberId}`);
   const thConfig = document.querySelector(`#config-${memberId}`);
 
+  // positionDataValue.innerText = '';
+  // thTask.innerText = '';
+  // thConfig.innerText = '';
+
   // 직책 - select 태그 조작
   const positionDataValue = thPosition.dataset.value;
-  thPosition.innerHTML = `<select>
-      <option value=1>팀원</option>
-      <option value=2>부리더</option>
-      <option value=3>리더</option>
-      </select>`;
-  for (let option of thPosition.childNodes[0]) {
-    if (option.value === positionDataValue) {
-      option.selected = true;
+
+  if (positionDataValue !== '3') {
+    thPosition.textContent = '';
+    thPosition.insertAdjacentHTML(
+      'beforeend',
+      `<select>
+        <option value=1>팀원</option>
+        <option value=2>부리더</option>
+        <option value=3>리더</option>
+        </select>`
+    );
+
+    for (let option of thPosition.childNodes[0]) {
+      if (option.value === positionDataValue) {
+        option.selected = true;
+      }
     }
   }
 
   // 담당업무 - input 태그 조작
   let thTaskValue = thTask.textContent;
-  thTask.innerHTML = `<input type='text' value='${thTaskValue}'/>`;
+  thTask.textContent = '';
+  thTask.insertAdjacentHTML(
+    'beforeend',
+    `<input type='text' value='${thTaskValue}'/>`
+  );
 
   // 설정: 수정버튼 > 적용버튼
-  thConfig.firstElementChild.innerHTML = `<button
-                class="myteam__memberList__update"
-                onclick="updateMemberApplicate(${memberId})"
-              >
-                ✅
-              </button>`;
+  thConfig.firstElementChild.textContent = '';
+  thConfig.firstElementChild.insertAdjacentHTML(
+    'beforeend',
+    `<button class="myteam__memberList__update" onclick="updateMemberApplicate(${memberId})">✅</button>`
+  );
 }
 
 // 수정한 내용 적용
 function updateMemberApplicate(memberId) {
   const select = document.querySelector(`#row-${memberId} select`);
   const inputTask = document.querySelector(`#row-${memberId} input`);
-  const position = Number(select.value);
+  let position = 3;
+  if (select) {
+    Number(select.value);
+  }
+
   const task = inputTask.value;
 
   fetch(url + `/${memberId}`, {
