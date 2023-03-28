c00b1c40737b07f3237baedca5c1deaca26f98c9
Jeongjiw00 <wldn0229@naver.com>
2023-03-28 16:46:56 +0900

    [수정] /project/ 경로일때 페이지 처리

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -151,6 +151,12 @@ class ProjectsController {
         .status(200)
         .render(page, { pageTitle, nextCursor, projects, search });
     } catch (error) {
+      if (error.message === 'url이 올바르지 않습니다.') {
+        return res.render('deletedTeam', {
+          pageTitle: 'Error',
+          pageContent: 'url이 올바르지 않습니다.',
+        });
+      }
       return res.status(500).json({ message: error.message });
     }
   };
