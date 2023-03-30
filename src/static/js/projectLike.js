fc8a0acfd525fc1eb28b3646dddb5f4a1d8cbd8b
rsl150101 <dnwnd501@naver.com>
2023-03-31 03:39:56 +0900

    [추가] 프로젝트 좋아요 html 변조 방지

--- src/static/js/projectLike.js
+++ src/static/js/projectLike.js
@@ -3,9 +3,19 @@ const projectLikeBtns = document.querySelectorAll('#projectLikeBtn');
 const handleProjectLikeBtn = async (btn) => {
   const id = btn.dataset.id;
   if (btn.classList.contains('project-like-btn--red')) {
-    await fetch(`/projects/${id}/like`, { method: 'DELETE' });
+    const { status } = await fetch(`/projects/${id}/like`, {
+      method: 'DELETE',
+    });
+
+    if (status === 403) {
+      window.location.reload();
+    }
   } else {
-    await fetch(`/projects/${id}/like`, { method: 'POST' });
+    const { status } = await fetch(`/projects/${id}/like`, { method: 'POST' });
+
+    if (status === 403) {
+      window.location.reload();
+    }
   }
   btn.classList.toggle('project-like-btn--red');
 };
